68f9b791bad218ce6a63e39ffef518bbde52e514
printfjoby <printfjoby@gmail.com>
2023-06-28 23:22:12 +0530

    added scripts

---
+++ scripts/deploy.js
@@ -0,0 +1,18 @@
+async function main() {
+    const [deployer] = await ethers.getSigners();
+  
+    console.log("Deploying contracts with the account:", deployer.address);
+
+    const Marketplace = await ethers.deployContract("Launchpad");
+
+    await Launchpad.waitForDeployment();
+  
+    console.log("Launchpad address:", await Launchpad.getAddress());
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
